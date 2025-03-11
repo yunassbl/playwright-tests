38c4bfe8bf9fb8771c8fc48570165c45e03f4a86
김정연 <yun@gimjeong-yeon-ui-MacBookPro.local>
2025-03-11 18:21:38 +0900

    pwa 서버 별 추가 수정

--- tests/test/search/uno_search_consultation_search.spec.js
+++ tests/test/search/uno_search_consultation_search.spec.js
@@ -10,42 +10,51 @@ test.use({
 });
 
 test('test', async ({ page }) => {
-  await page.goto('https://test.unocare.co.kr/login'); // 로그인 화면 진입
+  await page.goto('https://test.unocare.co.kr/login');
   // 로그인
   await expect(page.getByRole('img', { name: '고객을 관리하는 가장 좋은 선택 "UNO CRM"' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: '아이디(이메일)를 입력하세요' })).toBeVisible();
   await page.getByRole('textbox', { name: '아이디(이메일)를 입력하세요' }).click();
   await page.getByRole('textbox', { name: '아이디(이메일)를 입력하세요' }).fill('dev@test.com');
+  await expect(page.getByRole('textbox', { name: '비밀번호를 입력하세요' })).toBeVisible();
   await page.getByRole('textbox', { name: '비밀번호를 입력하세요' }).click();
   await page.getByRole('textbox', { name: '비밀번호를 입력하세요' }).fill('asdf1234!');
+  await expect(page.getByRole('button', { name: '로그인' })).toBeVisible();
   await page.getByRole('button', { name: '로그인' }).click();
-  // 메인 화면
+  // 메인 화면 진입
+  await page.waitForTimeout(3000);
   await expect(page.getByRole('button', { name: '로그아웃' })).toBeVisible();
+  await expect(page.getByRole('button', { name: '고객 조회' })).toBeVisible();
+  await page.getByRole('button', { name: '고객 조회' }).click();
+  // 고객 조회 진입
   await expect(page.getByRole('button', { name: '고객 조회' })).toBeVisible();
   await page.getByRole('button', { name: '고객 조회' }).click();
   // 고객 조회
   await expect(page.getByText('고객조회 내역')).toBeVisible();
+  await expect(page.getByRole('paragraph').filter({ hasText: '고객명' })).toBeVisible();
   await page.getByRole('textbox', { name: '고객명', exact: true }).click();
   await page.getByRole('textbox', { name: '고객명', exact: true }).fill('자동화');
+  await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
-  await expect(page.getByRole('cell', { name: '고객명', exact: true })).toBeVisible();
-  await expect(page.getByRole('button', { name: '고객명_자동화' })).toBeVisible();
-  await page.getByRole('button', { name: '고객명_자동화' }).click();
+  await expect(page.getByText('고객조회 내역')).toBeVisible();
+  await expect(page.getByRole('cell', { name: '고객명' }).nth(0)).toBeVisible();
+  await expect(page.getByRole('cell', { name: '자동화_신규고객' })).toBeVisible();
+  await page.getByRole('button', { name: '자동화_신규고객' }).click();
   // 통합차트
-  await expect(page.getByText('통합차트')).toBeVisible();
-  // 통합차트 > 상담
+  await expect(page.getByText('통합차트')).toBeVisible(); // 통합차트 진입
+  // 상담 진입
   await page.getByText('상담 (0)').click();
-  await expect(page.getByText('상담차트 (0)')).toBeVisible();
+  await expect(page.getByRole('button', { name: '+ 상담등록' })).toBeVisible();
   // 상담 등록
+  await expect(page.getByRole('button', { name: '+ 상담등록' })).toBeVisible();
   await page.getByRole('button', { name: '+ 상담등록' }).click();
-  await expect(page.getByText('상담 등록')).toBeVisible();
   // 상담사
-  await expect(page.locator('label').filter({ hasText: '상담사' })).toBeVisible();
   await page.getByRole('combobox', { name: '상담사를 선택하세요' }).click();
-  await page.getByRole('option', { name: '김정연' }).click();
+  await page.getByRole('option', { name: '최지안' }).click();
   // 시/수술 카테고리
   await expect(page.getByText('시/수술 카테고리')).toBeVisible();
   await page.getByRole('combobox', { name: '시/수술 카테고리를 선택하세요' }).click();
-  await page.getByRole('option', { name: '성형' }).click();
+  await page.getByRole('option', { name: '성형' }).nth(0).click();
   // 시/수술명
   await expect(page.locator('label').filter({ hasText: '시/수술명' })).toBeVisible();
   await page.getByRole('combobox', { name: '시/수술명을 선택하세요' }).click();
@@ -53,33 +62,36 @@ test('test', async ({ page }) => {
   // 상담결과
   await expect(page.locator('label').filter({ hasText: '상담결과' })).toBeVisible();
   await page.getByRole('combobox', { name: '-' }).click();
-  await page.getByRole('option', { name: '테스트결과3000' }).click();
+  await page.getByRole('option', { name: '해피콜' }).click();
   // 상담내용
   await expect(page.locator('label').filter({ hasText: '상담내용' })).toBeVisible();
   await expect(page.getByText('자주 쓰는 상용구')).toBeVisible();
   await page.getByRole('paragraph').filter({ hasText: /^$/ }).click();
-  await page.locator('pre div').first().fill('상담 내용 메모 자동화');
+  await page.locator('pre div').first().fill('상담 내용 입력 자동화');
   // 펜차트
   await expect(page.getByText('펜차트', { exact: true }).nth(1)).toBeVisible();
   await page.getByRole('button', { name: 'icon-image 펜차트 샘플함' }).click();
+  await expect(page.getByLabel('펜차트 샘플함').getByText('펜차트 샘플함')).toBeVisible();
   // 펜차트 > 펜차트 샘플함
   await expect(page.getByLabel('펜차트 샘플함').getByText('펜차트 샘플함')).toBeVisible();
-  await expect(page.getByLabel('자동화').getByText('자동화')).toBeVisible();
-  // 펜차트 샘플함 > 폴더 진입
-  await page.getByLabel('자동화').locator('div').dblclick();
-  await expect(page.getByText('자동화 샘플.jpg')).toBeVisible();
-  await page.getByLabel('자동화 샘플.jpg').getByRole('checkbox').check();
-  await expect(page.getByText('자동화 샘플2.jpg')).toBeVisible();
-  await page.getByLabel('자동화 샘플2.jpg').getByRole('checkbox').check();
+  await page.getByLabel('고객상담차트.jpg').getByRole('checkbox').check();
+  await expect(page.getByLabel('펜차트 샘플함').getByText('펜차트 샘플함')).toBeVisible();
+  await expect(page.getByRole('button', { name: '불러오기' })).toBeVisible();
   await page.getByRole('button', { name: '불러오기' }).click();
-  await expect(page.getByText('차트를 불러왔습니다')).toBeVisible();
-  // 상담 등록
   await expect(page.getByText('상담 등록')).toBeVisible();
-  await expect(page.getByText('자동화 샘플.jpg')).toBeVisible();
-  await expect(page.getByText('자동화 샘플2.jpg')).toBeVisible();
+  await expect(page.getByText('고객상담차트.jpg')).toBeVisible();
+  await expect(page.getByRole('button', { name: '저장' })).toBeVisible();
   await page.getByRole('button', { name: '저장' }).click();
-  // 상담 등록 완료
+  await page.waitForTimeout(3000);
   await expect(page.getByText('상담을 생성했습니다')).toBeVisible();
+  // 상담 등록 확인
+  await expect(page.getByText('상담차트 (1)')).toBeVisible();
+  await expect(page.getByRole('cell', { name: '상담 내용 입력 자동화' })).toBeVisible(); // 상담 내용
+  await expect(page.getByRole('cell', { name: '최지안' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '성형' }).nth(2)).toBeVisible();
+  await expect(page.getByRole('cell', { name: '눈매교정' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '해피콜' })).toBeVisible(); // 상담결과
+  await expect(page.getByRole('cell', { name: '건' })).toBeVisible(); // 펜차트
   // 통합차트 닫기
   await expect(page.getByText('통합차트')).toBeVisible();
   await page.locator('div').filter({ hasText: /^3D Meta-Vu$/ }).getByRole('button').nth(2).click();
@@ -95,8 +107,8 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: '고객명', exact: true }).fill('자동화');
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '고객명', exact: true })).toBeVisible();
-  await expect(page.getByRole('button', { name: '고객명_자동화' })).toBeVisible();
-  await page.getByRole('button', { name: '고객명_자동화' }).click();
+  await expect(page.getByRole('button', { name: '자동화_신규고객' })).toBeVisible();
+  await page.getByRole('button', { name: '자동화_신규고객' }).click();
   // 고객명 검색 > 통합차트 진입
   await expect(page.getByText('통합차트', { exact: true })).toBeVisible();
   // 고객명 검색 > 통합차트 닫기
@@ -109,10 +121,10 @@ test('test', async ({ page }) => {
   // 차트번호 검색
   await expect(page.getByRole('paragraph').filter({ hasText: '차트번호' })).toBeVisible();
   await page.getByRole('textbox', { name: '차트번호' }).click();
-  await page.getByRole('textbox', { name: '차트번호' }).fill('1234567890');
+  await page.getByRole('textbox', { name: '차트번호' }).fill('1234568');
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '차트번호' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '1234567890' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '1234568' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -121,17 +133,17 @@ test('test', async ({ page }) => {
   // 전화번호 검색
   await expect(page.getByRole('paragraph').filter({ hasText: '전화번호' })).toBeVisible();
   await page.getByRole('textbox', { name: '전화번호 뒤 4자리' }).click();
-  await page.getByRole('textbox', { name: '전화번호 뒤 4자리' }).fill('9998');
+  await page.getByRole('textbox', { name: '전화번호 뒤 4자리' }).fill('3535');
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '전화번호' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '-9999-9998' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '-3535-3535' })).toBeVisible();
   // 전화번호 > 메시지 전송
-  await page.getByRole('cell', { name: '-9999-9998' }).getByRole('button').click();
+  await page.getByRole('cell', { name: '-3535-3535' }).getByRole('button').click();
   await expect(page.getByRole('heading', { name: '메시지 전송' })).toBeVisible();
   await page.locator('.send-message-wrapper > div > div:nth-child(2) > .MuiButtonBase-root').click();
   await expect(page.getByText('상담조회 내역')).toBeVisible();
   // 전화번호 > 메시지 전송 (카카오톡)
-  await page.getByRole('cell', { name: '-9999-9998' }).locator('path').nth(1).click();
+  await page.getByRole('cell', { name: '-3535-3535' }).locator('path').nth(1).click();
   await expect(page.getByRole('heading', { name: '메시지 전송' })).toBeVisible();
   await page.getByRole('button').filter({ hasText: /^$/ }).first().click();
   await expect(page.getByText('상담조회 내역')).toBeVisible();
@@ -185,11 +197,11 @@ test('test', async ({ page }) => {
   // 상세 필터 > 상담사 검색
   await expect(page.getByText('상담사').nth(1)).toBeVisible();
   await page.locator('div:nth-child(4) > .MuiFormControl-root > .MuiInputBase-root').first().click();
-  await page.getByRole('option', { name: '김정연' }).click();
+  await page.getByRole('option', { name: '최지안' }).click();
   await page.getByRole('button', { name: 'Close' }).click();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '상담사' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '김정연' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '최지안' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -239,11 +251,11 @@ test('test', async ({ page }) => {
   // 상세 필터 > 상담결과 검색
   await expect(page.getByText('상담결과').first()).toBeVisible();
   await page.locator('div:nth-child(2) > div:nth-child(2) > .MuiFormControl-root > .MuiInputBase-root').first().click();
-  await page.getByRole('option', { name: '테스트결과3000' }).click();
+  await page.getByRole('option', { name: '해피콜' }).click();
   await page.getByRole('button', { name: 'Close' }).click();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '상담결과' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '테스트결과' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '해피콜' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -320,11 +332,11 @@ test('test', async ({ page }) => {
   // 상세 필터 > 소개자 검색
   await expect(page.getByRole('paragraph').filter({ hasText: /^소개자$/ })).toBeVisible();
   await page.getByRole('combobox', { name: '소개자' }).click();
-  await page.getByRole('combobox', { name: '소개자' }).fill('장지훈');
-  await page.getByRole('option', { name: '장지훈1124 1990-02-02 남성 019-8888-' }).click();
+  await page.getByRole('combobox', { name: '소개자' }).fill('김정연');
+  await page.getByText('-8989-8989').click();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '소개자' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '장지훈' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '김정연' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -345,7 +357,7 @@ test('test', async ({ page }) => {
   await page.getByRole('option', { name: '1995년' }).click();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '생년월일' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '-07-06' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '-05-05' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -354,14 +366,14 @@ test('test', async ({ page }) => {
   // 상세 필터 > 고객정보 필터 > 생일 검색
   await expect(page.getByText('생일')).toBeVisible();
   await page.getByRole('button', { name: '월' }).nth(1).click();
-  await page.getByRole('option', { name: '07월' }).click();
+  await page.getByRole('option', { name: '05월' }).click();
   await expect(page.getByText('생일')).toBeVisible();
   await page.getByRole('button', { name: '일' }).nth(1).click();
-  await page.getByRole('option', { name: '06일' }).click();
+  await page.getByRole('option', { name: '05일' }).click();
   await expect(page.getByText('생일')).toBeVisible();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '생년월일' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '-07-06' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '-05-05' })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -396,6 +408,7 @@ test('test', async ({ page }) => {
   await page.getByRole('radio', { name: '남성' }).check();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '성별' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '여성' }).first()).not.toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -406,7 +419,7 @@ test('test', async ({ page }) => {
   await expect(page.locator('div:nth-child(5) > .MuiFormGroup-root > label > .MuiTypography-root').first()).toBeVisible();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '내/외국인' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '내국인' }).first()).toBeVisible();
+  await expect(page.getByRole('cell', { name: '외국인', exact: true })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -418,7 +431,7 @@ test('test', async ({ page }) => {
   await page.getByRole('radio', { name: '내국인' }).check();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '내/외국인' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: '내국인' }).first()).toBeVisible();
+  await expect(page.getByRole('cell', { name: '외국인', exact: true })).not.toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
@@ -430,6 +443,7 @@ test('test', async ({ page }) => {
   await page.getByRole('radio', { name: '외국인' }).check();
   await page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' }).click();
   await expect(page.getByRole('cell', { name: '내/외국인' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: '외국인', exact: true })).toBeVisible();
   // 초기화
   await page.getByRole('button', { name: '초기화' }).click();
   await expect(page.locator('#bodyContentsWrapper').getByRole('button', { name: '조회' })).toBeVisible();
